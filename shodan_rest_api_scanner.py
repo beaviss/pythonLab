26f0ed90e30df14fed367d41189c16f2af23ebdf
BeaVisS <kazcrust@gmail.com>
2019-08-07 21:02:49 +0300

    pure web api requests

---
+++ shodan_rest_api_scanner.py
@@ -0,0 +1,14 @@
+import requests
+# import json
+
+SHODAN_API_KEY = str(input('Input your Shodan API key: '))
+# host_ip = str(input('Input interesting host IP: '))
+host_name = str(input('Input domain name: '))
+ip_addr = requests.get('https://api.shodan.io/dns/resolve?hostnames=' + host_name + '&key=' + SHODAN_API_KEY)
+response = requests.get('https://api.shodan.io/shodan/host/' + ip_addr.json()[host_name] + '?key=' + SHODAN_API_KEY)
+
+# print(response.json())
+print('Country: {}'.format(response.json()['country_name']))
+# # print('Hostname: {}'.format(response['hostnames'][0]))
+print('Last Update: {}'.format(response.json()['last_update']))
+print('Open Ports: {}'.format(response.json()['ports']))
